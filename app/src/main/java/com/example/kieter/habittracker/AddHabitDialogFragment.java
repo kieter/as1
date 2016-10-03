e14a102cf9e4870a24d3d35deb791a0f6fe8b4cb
Kieter Philip Balisnomo <balisnom@ualberta.ca>
2016-10-03 02:21:13 -0600

    Commented AddHabitDialogFragment.java and Listener.java

--- app/src/main/java/com/example/kieter/habittracker/AddHabitDialogFragment.java
+++ app/src/main/java/com/example/kieter/habittracker/AddHabitDialogFragment.java
@@ -43,6 +43,12 @@ import java.util.Locale;
  * Created by kieter on 9/23/2016.
  */
 
+/*
+AddHabitDialog fragment is a custom dialog fragment that allows the user to input a new habit's
+name, frequency (SMTWRFS),  and date created (using a calendar, automatically filed) and passes
+inputted information back to MainActivity to create an actual Habit.
+*/
+    //TODO: put all the stack overflow things in a readme
 //https://developer.android.com/guide/topics/ui/dialogs.html
 //http://stackoverflow.com/questions/10903754/input-text-dialog-android
 public class AddHabitDialogFragment extends DialogFragment {
@@ -51,6 +57,9 @@ public class AddHabitDialogFragment extends DialogFragment {
         // required empty constructor
     }
     //http://android-developers.blogspot.ca/2012/05/using-dialogfragments.html
+    /*
+    Calls the method that creates an instance of the habit using the inputted information.
+     */
     public interface HabitInputDialogListener {
         void onFinishInputEvent(String name, String date, ArrayList<String> selectedDays);
     }
@@ -58,8 +67,11 @@ public class AddHabitDialogFragment extends DialogFragment {
     private HabitInputDialogListener listener;
 
     @Override
+    /*
+    onCreateDialog will set up all the views of the dialog for editting
+     */
     public Dialog onCreateDialog(final Bundle savedInstanceState) {
-        // Create dialog
+        // create dialog
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         // set dialog title
         builder.setTitle("Add new habit");
@@ -77,6 +89,9 @@ public class AddHabitDialogFragment extends DialogFragment {
         input.setOnKeyListener(new View.OnKeyListener() {
             //http://stackoverflow.com/questions/7940765/how-to-hide-the-soft-keyboard-from-inside-a-fragment
             @Override
+            /*
+            When the user hits enter the keyboard is brought down.
+             */
             public boolean onKey(View v, int keyCode, KeyEvent event) {
                 if (event.getAction() == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
                     InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
@@ -84,11 +99,11 @@ public class AddHabitDialogFragment extends DialogFragment {
                     return true;
                 }
                 return false;
-            }
-        });
+            } // end of onKey
+        }); // end of onKeyListener
 
 
-        //set Calendar and date input
+        // set Calendar and date input
         final Calendar habitCalendar = Calendar.getInstance();
         final EditText dateInput = new EditText(getActivity());
         dateInput.setHint("Start date");
@@ -96,12 +111,15 @@ public class AddHabitDialogFragment extends DialogFragment {
         layout.addView(dateInput);
         updateCalendarLabel(dateInput, habitCalendar);
 
-        //set multichoice list
+        // set multichoice list
         final CharSequence[] daysOfWeek = new CharSequence[] {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
         final ArrayList<String> selectedDaysofWeek = new ArrayList();
 
         final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
             @Override
+            /*
+            onDateSet opens a calendar that allows the user to pick a date of creation.
+             */
             public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
                 habitCalendar.set(Calendar.YEAR, year);
                 habitCalendar.set(Calendar.MONTH, monthOfYear);
@@ -112,8 +130,10 @@ public class AddHabitDialogFragment extends DialogFragment {
 
         //http://stackoverflow.com/questions/14933330/datepicker-how-to-popup-datepicker-when-click-on-edittext
         dateInput.setOnTouchListener(new View.OnTouchListener() {
-
             @Override
+            /*
+            When the user touches the edit text date a calendar pops up.
+             */
             public boolean onTouch(View v, MotionEvent event) {
                 if (event.getAction() == MotionEvent.ACTION_DOWN) {
                     DatePickerDialog habitDatePicker = new DatePickerDialog(getActivity(), date, habitCalendar
@@ -129,6 +149,9 @@ public class AddHabitDialogFragment extends DialogFragment {
         //https://developer.android.com/guide/topics/ui/dialogs.html
         builder.setMultiChoiceItems(daysOfWeek, null, new DialogInterface.OnMultiChoiceClickListener() {
             @Override
+            /*
+            The user is able to select the weekly frequency of the Habit
+             */
             public void onClick(DialogInterface dialog, int which, boolean isChecked) {
                 if (isChecked) {
                     selectedDaysofWeek.add(daysOfWeek[which].toString());
@@ -143,30 +166,35 @@ public class AddHabitDialogFragment extends DialogFragment {
         // okay button
         builder.setPositiveButton("Okay", new DialogInterface.OnClickListener() {
             @Override
+            /*
+            Affirmative button saves the user's input and returns to the MainActivity
+             */
             public void onClick(DialogInterface dialog, int which) {
-
-
                 HabitInputDialogListener listener = (HabitInputDialogListener) getActivity();
                 listener.onFinishInputEvent(input.getText().toString(), dateInput.getText().toString(), selectedDaysofWeek);
 
                 dialog.dismiss();
-
             }
         });
 
         // cancel button
         builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
             @Override
+            /*
+            Cancels the dialog
+             */
             public void onClick(DialogInterface dialog, int which) {
                 dialog.cancel();
             }
         });
-
         builder.setView(layout);
-
         return builder.create();
-    }
+    } // end of onCreate
 
+    /*
+    updateCalendarLable will update the text automatically entered into the EditText for inputting
+    date
+     */
     private void updateCalendarLabel(EditText dateInput, Calendar habitCalendar) {
         String dateFormat = "yyyy/MM/dd";
         SimpleDateFormat sdf = new SimpleDateFormat(dateFormat, Locale.CANADA);
@@ -174,4 +202,4 @@ public class AddHabitDialogFragment extends DialogFragment {
     }
 
 
-}
+} // end of AddHabitDialogFragment
--- app/src/main/java/com/example/kieter/habittracker/Listener.java
+++ app/src/main/java/com/example/kieter/habittracker/Listener.java
@@ -18,10 +18,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 package com.example.kieter.habittracker;
 
-/**
- * Created by kiete on 9/25/2016.
- */
-
 public interface Listener {
     public void update();
 }
