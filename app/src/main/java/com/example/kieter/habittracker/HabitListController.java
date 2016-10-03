dbc29c71722189aff9aaa6bb02dbf5b2d6c3aec9
Kieter Philip Balisnomo <balisnom@ualberta.ca>
2016-10-03 10:30:18 -0600

    Commented HabitListController.java

--- app/src/main/java/com/example/kieter/habittracker/HabitListController.java
+++ app/src/main/java/com/example/kieter/habittracker/HabitListController.java
@@ -17,7 +17,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package com.example.kieter.habittracker;
-
+/*
+HabitListController is a singleton of habitList, it manages any modification to the habit list
+including adds and deletes.
+Once the user has added a habit, and selects it from the list (or multiple habits and selects one)
+the controller is passed the selected habit so the user can see it in the habit activity.
+ */
 public class HabitListController {
     private static HabitList habitList = null;
     private static Habit selectedHabit;
@@ -43,14 +48,17 @@ public class HabitListController {
         return getHabitList().size();
     }
 
+    // a setter that sets the habitList
     public static void giveHabitList(HabitList givenHabitList) {
         habitList = givenHabitList;
     }
 
+    // a setter that sets the selected habit
     public static void giveSelectedHabit(Habit habit) {
         selectedHabit = habit;
     }
 
+    // a getter that returns the selected habit
     public static Habit getSelectedHabit() {
         return selectedHabit;
     }
